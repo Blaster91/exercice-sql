6f8491d691b595828b3073a3a3b2f7351d0dc41c
Romain Leroy <r.leroy@codeur.online>
2019-09-24 15:08:14 +0200

    commande sql réponse 1,2,3

--- index.php
+++ index.php
@@ -1,31 +1,74 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>requétes SQL</title>
+  </head>
+  <body>
+    <header style="height: 100px ;width:100%;">
+    </header>
+    <section id ="requetes-sql" style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+
+      <FORM style="margin-bottom: 25px;">
+      <SELECT name="nom" size="1">
+        <OPTION>Afficher tous les gens dont le nom est palmer
+        <OPTION>Afficher toutes les femmes
+        <OPTION>Tous les états dont la lettre commence par N
+        <OPTION>Tous les emails qui contiennent google
+        <OPTION>Répartition par Etat et le nombre d’enregistrement par état (croissant)
+        <OPTION>Insérer un utilisateur, lui mettre à jour son adresse mail puis supprimer l’utilisateur
+        <OPTION>Nombre de femme et d’homme
+        <OPTION>Afficher l'âge de chaque personne, puis la moyenne d’âge des femmes et des hommes
+      </SELECT>
+      </FORM>
+      <?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
+
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
+
+    function beginChildren() {
+        echo "<tr>";
+    }
+
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "mydb";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT id, first_name, last_name FROM board WHERE last_name = 'palmer'");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
 }
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com');";
-$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('Mary', 'Moe', 'mary@example.com');";
-$sql .= "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('Julie', 'Dooley', 'julie@example.com')";
-
-if ($conn->multi_query($sql) === TRUE) {
-    echo "New records created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
-
-
-$conn->close();
-
 $conn = null;
+echo "</table>";
 ?>
+
+
+
+    </section>
+  </body>
+</html>
--- sqlselect.php
+++ sqlselect.php
@@ -1,45 +1,4 @@
-<?php
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
-
-class TableRows extends RecursiveIteratorIterator {
-    function __construct($it) {
-        parent::__construct($it, self::LEAVES_ONLY);
-    }
-
-    function current() {
-        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-    }
-
-    function beginChildren() {
-        echo "<tr>";
-    }
-
-    function endChildren() {
-        echo "</tr>" . "\n";
-    }
-}
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mydb";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests");
-    $stmt->execute();
-
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-        echo $v;
-    }
-}
-catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-$conn = null;
-echo "</table>";
-?>
+Afficher tous les gens dont le nom est palmer: SELECT id, first_name, last_name FROM board WHERE last_name = 'palmer'
+Afficher toutes les femmes: SELECT id, first_name, last_name, gender FROM `board` WHERE gender = "female"
+Tous les états dont la lettre commence par N: SELECT state_code FROM `board` WHERE state_code LIKE 'n%'
+Tous les emails qui contiennent google: 
